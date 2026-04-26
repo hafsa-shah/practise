ccdfa6656a2d02ddb8c0f3a381e59edacafd9982
hafsa29 <hafsaazad29@gmail.com>
2026-04-26 16:20:33 +0530

    plinth

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "man8" {
     name = "tim8"
     location = "east us"
 }
+
+resource "azurerm_resource_group" "name67" {
+    name = "point66"
+    location = "central india"
+  
+}
